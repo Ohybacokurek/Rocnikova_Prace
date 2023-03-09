8f74484adcbf02cd54b392656ac7cffe7c95b06e
Ohybacokurek <92986338+Ohybacokurek@users.noreply.github.com>
2023-03-09 17:33:30 +0100

    Add files via upload

---
+++ Roc/res/movement.js
@@ -0,0 +1,13 @@
+addEventListener("keydown", function(e){
+    if (e.code == "KeyD") vxr = 5;
+    if (e.code == "KeyA") vxl = -5;
+    if (e.code == "KeyW") vyu = -5;
+    if (e.code == "KeyS") vyd = 5;
+})
+
+addEventListener("keyup", function(e){
+    if (e.code == "KeyD") vxr = 0;
+    if (e.code == "KeyA") vxl = 0;
+    if (e.code == "KeyW") vyu = 0;
+    if (e.code == "KeyS") vyd = 0;
+})
